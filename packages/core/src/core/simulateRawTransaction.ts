15f92c43de1ea4b1b9e722871bfc685a4fdf5877
Christian <contact@crusso.ch>
2025-06-30 13:35:56 +0200

    Correction de bugs

--- packages/core/src/core/simulateRawTransaction.ts
+++ packages/core/src/core/simulateRawTransaction.ts
@@ -1,31 +1,34 @@
-import { Connection, PublicKey, SimulatedTransactionResponse, Transaction } from '@solana/web3.js';
+import {
+    Connection,
+    PublicKey,
+    SimulatedTransactionResponse,
+    VersionedTransaction,
+    SimulateTransactionConfig
+} from '@solana/web3.js'
 
-// Simulate a signed, serialized transaction before broadcasting
+/**
+ * Simule une transaction signée et sérialisée avant envoi.
+ */
 export async function simulateRawTransaction(
     connection: Connection,
     rawTransaction: Buffer,
-    includeAccounts?: boolean | Array<PublicKey>
+    includeAccounts?: boolean | PublicKey[]
 ): Promise<SimulatedTransactionResponse> {
-    /*
-       Simulating a transaction directly can cause the `signatures` property to change.
-       Possibly related:
-       https://github.com/solana-labs/solana/issues/21722
-       https://github.com/solana-labs/solana/pull/21724
-       https://github.com/solana-labs/solana/issues/20743
-       https://github.com/solana-labs/solana/issues/22021
+    const transaction = VersionedTransaction.deserialize(rawTransaction);
 
-       Clone it from the bytes instead, and make sure it's likely to succeed before paying for it.
+    const config: SimulateTransactionConfig = {
+        sigVerify: false,
+        replaceRecentBlockhash: false,
+        accounts: Array.isArray(includeAccounts)
+            ? {
+                  encoding: 'base64' as const, // <- 💥 Fix ici
+                  addresses: includeAccounts.map((pk) => pk.toBase58())
+              }
+            : undefined
+    };
 
-       Within simulateTransaction there is a "transaction instanceof Transaction" check. Since connection is passed
-       from outside the library, it uses parent application's version of web3.js. "instanceof" won't recognize a match.
-       Instead, let's explicitly call for simulateTransaction within the dependency of the library.
-     */
-    const simulated = await Connection.prototype.simulateTransaction.call(
-        connection,
-        Transaction.from(rawTransaction),
-        undefined,
-        includeAccounts
-    );
+    const simulated = await connection.simulateTransaction(transaction, config);
+
     if (simulated.value.err) throw new Error('Simulation error');
 
     return simulated.value;
